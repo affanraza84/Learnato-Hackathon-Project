f44619b520f2ec8632f0775cd2212cd6eebbbafb
Affan Raza <affanraza@Affans-MacBook-Air.local>
2025-11-10 19:21:35 +0530

    changed url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'https://learnato-hackathon-project-backend.onrender.com';
+const API_BASE = import.meta.env.VITE_API_URL || 'https://learnato-hackathon-project-backend.onrender.com/api';
 
 export const api = axios.create({
     baseURL: API_BASE,
